55944c8114bed7dc64c45c99b82a9c9c23737fb9
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-10-08 19:33:31 -0800

    functions that must be defined included in main file. location change later

--- fifo-lifo/fifo-lifo.cpp
+++ fifo-lifo/fifo-lifo.cpp
@@ -5,8 +5,30 @@
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
+
+
+//First-in First-Out
+void FifoPush(vector<string>& container, const string& item);
+void FifoPop(vector<string>& container, string& item);
+
+//Last-In First-Out
+void LifoPush(vecotr<string>& container, const string& item);
+void LifoPop(vector<string>& container, string& item);
+
+//Shared Functionality
+bool IsContainerEmpty(const vector<string>& container);
+void PrintContainer(const vector<string>& container);
+
+//Implememnt these two tests to verify your functions work 
+//with at least the sequence:
+//push "A', push "B", push "c', push "d"
+// pop , pop, pop, pop
+bool TestFifo();
+bool TestLifo();
