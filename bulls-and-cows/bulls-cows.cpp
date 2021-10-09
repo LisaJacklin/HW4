c8ca8798660e273d7339c36db76e5fb6e2414fe3
Lisa Jacklin <lisaloveslola09@outlook.com>
2021-10-08 19:12:37 -0800

    code set up but needs to separate to correct source files. Adjust generate secretnumber to vector...

---
+++ bulls-and-cows/bulls-cows.cpp
@@ -0,0 +1,7 @@
+//bulls-cows.cpp
+//Lisa Jacklin
+//CS 201
+//10/8/2021
+
+#include <iostream>
+#include "bulls-cows.hpp"
---
+++ bulls-and-cows/bulls-cows.hpp
@@ -0,0 +1,9 @@
+//bulls-cows.cpp
+//Lisa Jacklin
+//CS 201
+//10/8/2021
+
+#ifndef BULLS_COWS_HPP
+#define BULLS_COWS_HPP
+
+#endif
